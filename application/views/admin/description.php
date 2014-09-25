d504b90f31b384ffec01f32ad87b7d45da8a6f99
Odarchenko N.D <odarchenko.n.d@gmail.com>
2014-09-25 16:08:57 +0300

    возможность редактирования meta на странице редактирования описания и странице категорий

--- application/views/admin/description.php
+++ application/views/admin/description.php
@@ -1,13 +1,19 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');?>
 <style type="text/css">a{color: #000;}</style>
-<h2>Редактирование описания:<br> <a href="<?php echo $link; ?>"><?php echo $productname; ?></a></h2>
+<h2>Редактирование описания:<br> <a id="prod_link" href="<?php echo $link; ?>"><?php echo $productname; ?></a></h2>
 
 <form action="" method="post">    
     <p>
         <textarea cols="80" id="editor1" name="editor" rows="10"><?php echo $html;?></textarea>
         <script type="text/javascript" src="<?php echo url::base();?>js/ckedit/inc.js"></script>
     </p>
-    <p>
-        <input type="submit" value="Сохранить" />
+    <p class="text-center">
+        <input class="btn btn-lg" type="submit" value="Сохранить" />
     </p>
 </form>
+
+<?php echo View::factory('admin/editMeta'); ?>
+
+<div class="text-center">
+    <button class="btn btn-lg" onclick="edit_meta_by_path($('#prod_link').attr('href'));">Редактировать title и meta</button>
+</div>
