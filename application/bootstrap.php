745de7a3ef1e76485d140fc1aa7da08cde2c3556
Odarchenko N.D <odarchenko.n.d@gmail.com>
2015-01-22 20:50:37 +0200

    Релиз v1.7.2

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -54,7 +54,7 @@ setlocale(LC_ALL, 'ru_RU.utf-8');
 /**
  * Версия php5shop
  */
-define('VERSION', '1.7.1');
+define('VERSION', '1.7.2');
 
 spl_autoload_register(array('Kohana', 'auto_load'));
 ini_set('unserialize_callback_func', 'spl_autoload_call');
